aa0102b3dd98937080849e57dc84726c79fe5ed1
drfalcoew <drfalcoew@gmail.com>
2018-10-15 15:16:11 -0700

    Changed UIViews with SKSpriteNodes for the UI Added movement actions for User

--- HeroOne/HeroOne/GameScene.swift
+++ HeroOne/HeroOne/GameScene.swift
@@ -9,19 +9,65 @@
 import SpriteKit
 import GameplayKit
 
+
+
+struct BitMasks {
+    
+}
+
 class GameScene: SKScene {
     
+    var user : SKSpriteNode!
+    var floor : SKSpriteNode!
+    
+    
+    var leftBtn : SKSpriteNode!
+    var rightBtn : SKSpriteNode!
+    var upBtn : SKSpriteNode!
+    
+    var userLeft : Bool = false
+    var userRight : Bool = false
     
     override func didMove(to view: SKView) {
         
-     
+        floor = childNode(withName: "floor") as? SKSpriteNode
+        user = childNode(withName: "user") as? SKSpriteNode
+        leftBtn = childNode(withName: "left") as? SKSpriteNode
+        rightBtn = childNode(withName: "right") as? SKSpriteNode
+        upBtn = childNode(withName: "up") as? SKSpriteNode
         
+        SetupControls()
+        SetupConstraints()
+        SetupPhysicsBodies()
+    }
+
+    func SetupControls() {
       
+    }
+    
+    func SetupConstraints() {
+    
+    }
+    
+    func SetupPhysicsBodies() {
+        floor?.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: floor.frame.width, height: floor.frame.height - floor.frame.height * 0.26))
+        floor.physicsBody?.isDynamic = true
+        floor.physicsBody?.pinned = true
+        floor.physicsBody?.collisionBitMask = 0
+        floor.physicsBody?.categoryBitMask = 1
+        
+        user.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: user.frame.width, height: user.frame.height))
+        user.physicsBody?.isDynamic = true
+        user.physicsBody?.pinned = false
+        user.physicsBody?.allowsRotation = false
+        user.physicsBody?.affectedByGravity = true
+        user.physicsBody?.collisionBitMask = 1
+        user.physicsBody?.categoryBitMask = 0
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
@@ -32,8 +78,40 @@ class GameScene: SKScene {
      
     }
     
+    func updateUser(direction: String) {
+        if direction == "left" {
+            userLeft = true
+            user.xScale = 1
+            userRight = false
+            runUser()
+        }
+        else if direction == "right" {
+            userRight = true
+            user.xScale = -1
+            userLeft = false
+            runUser()
+        }
+    }
+    
+    
+    func runUser() {
+        //setup walkAnimation here
+    }
+    
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        for t in touches { self.touchDown(atPoint: t.location(in: self)) 
+            let position = t.location(in: self)
+            let touchedNode = self.atPoint(position)
+            if let name = touchedNode.name {
+                if name == "right" {
+                    updateUser(direction: "right")
+                } else if name == "left" {
+                    updateUser(direction: "left")
+                }
+            }
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -41,15 +119,34 @@ class GameScene: SKScene {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for t in touches { self.touchUp(atPoint: t.location(in: self))
+            cancelMovements()
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        for t in touches { self.touchUp(atPoint: t.location(in: self))
+            cancelMovements()
+            
+        }
     }
     
     
+    func cancelMovements() {
+        userLeft = false
+        userRight = false
+    }
+    
+    func moveUser() {
+        if userLeft {
+            user.position.x -= 2.0
+        } else if userRight {
+            user.position.x -= -2.0
+        }
+    }
+    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        moveUser()
     }
 }
--- HeroOne/HeroOne/ViewControllers/GameViewController.swift
+++ HeroOne/HeroOne/ViewControllers/GameViewController.swift
@@ -42,9 +42,9 @@ class GameViewController: UIViewController {
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
+            return .landscapeRight
         } else {
-            return .all
+            return .landscapeRight
         }
     }
 
