653438587022f110f9f225b435e6b983638e37b2
drfalcoew <drfalcoew@gmail.com>
2018-10-27 21:45:58 -0700

    Added attack

--- HeroOne/HeroOne/GameScene.swift
+++ HeroOne/HeroOne/GameScene.swift
@@ -15,11 +15,11 @@ struct BitMasks {
     
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var user : SKSpriteNode!
     var floor : SKSpriteNode!
-    var userSize : CGSize = CGSize(width: 55, height: 120)
+    var userSize : CGSize = CGSize(width: 81, height: 140)
     
     var leftBtn : SKSpriteNode!
     var rightBtn : SKSpriteNode!
@@ -28,7 +28,19 @@ class GameScene: SKScene {
     var userLeft : Bool = false
     var userRight : Bool = false
     
+    var attackStart : CGPoint!
+    var attack = false
+    
+    var fireball : SKSpriteNode!
+    var fireballAtlas = SKTextureAtlas(named: "Fireball")
+    var fireballArray = [SKTexture]()
+    
+    var tempNode : SKSpriteNode!
+    
+    
     override func didMove(to view: SKView) {
+        
+        
         
         floor = childNode(withName: "floor") as? SKSpriteNode
         user = childNode(withName: "user") as? SKSpriteNode
@@ -36,13 +48,27 @@ class GameScene: SKScene {
         rightBtn = childNode(withName: "right") as? SKSpriteNode
         upBtn = childNode(withName: "up") as? SKSpriteNode
         
-        SetupControls()
+        physicsWorld.gravity = CGVector(dx: 0, dy: -9.8)
+        physicsWorld.contactDelegate = self as! SKPhysicsContactDelegate
+        self.view?.isMultipleTouchEnabled = true
+        
+        
+        SetupWeaponType()
+        SetupAnimations()
         SetupConstraints()
         SetupPhysicsBodies()
     }
 
-    func SetupControls() {
-      
+    
+    func SetupWeaponType() {
+        let fireballTexture = SKTexture(imageNamed: "1")
+        fireball = SKSpriteNode(texture: fireballTexture, size: CGSize(width: user.size.height, height: user.size.height * 0.384))
+    }
+    
+    func SetupAnimations() {
+        for i in 1...fireballAtlas.textureNames.count {
+            fireballArray.append(SKTexture(imageNamed: "\(i)"))
+        }
     }
     
     func SetupConstraints() {
@@ -50,7 +76,7 @@ class GameScene: SKScene {
     }
     
     func SetupPhysicsBodies() {
-        floor?.physicsBody = SKPhysicsBody(rectangleOf: floor.size)
+        floor?.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: floor.frame.width, height: floor.frame.height - floor.frame.height * 0.26))
         floor.physicsBody?.isDynamic = true
         floor.physicsBody?.pinned = true
         floor.physicsBody?.collisionBitMask = 0
@@ -63,7 +89,19 @@ class GameScene: SKScene {
         user.physicsBody?.affectedByGravity = true
         user.physicsBody?.collisionBitMask = 1
         user.physicsBody?.categoryBitMask = 0
+        
+        fireball.physicsBody = SKPhysicsBody(texture: SKTexture(imageNamed: "1"), size: fireball.size)
+        fireball.physicsBody?.isDynamic = true
+        fireball.physicsBody?.pinned = false
+        fireball.physicsBody?.allowsRotation = true
+        fireball.physicsBody?.affectedByGravity = true
+        fireball.physicsBody?.collisionBitMask = 1
+        fireball.physicsBody?.categoryBitMask = 2
+        fireball.physicsBody?.density = 30
+        
+        
     }
+    
     
     
     func touchDown(atPoint pos : CGPoint) {
@@ -85,7 +123,6 @@ class GameScene: SKScene {
             user.size = userSize
             userRight = false
             runUser()
-            print(user.size)
         }
         else if direction == "right" {
             userRight = true
@@ -94,8 +131,27 @@ class GameScene: SKScene {
             user.size = userSize
             userLeft = false
             runUser()
-            print(user.size)
         }
+    }
+    
+    
+    func Fire(dx : CGFloat, dy : CGFloat) {
+        
+        print(dx, dy)
+        if dx > 100 || dx < -100 || dy > 100 || dy < -100 {
+            if let x = fireball {
+                x.position = user.position
+                tempNode = (x.copy() as! SKSpriteNode)
+                tempNode?.physicsBody?.affectedByGravity = false
+                tempNode?.name = "fireball"
+                self.addChild(tempNode!)
+                tempNode?.run(SKAction.fadeIn(withDuration: 0.1))
+                tempNode.physicsBody?.applyImpulse(CGVector(dx: dx, dy: dy))
+                tempNode.run(SKAction.repeatForever(SKAction.animate(with: fireballArray, timePerFrame: 0.2)))
+                attack = false
+            }
+        }
+        
     }
     
     
@@ -104,9 +160,9 @@ class GameScene: SKScene {
     }
     
     
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) 
+        for t in touches { self.touchDown(atPoint: t.location(in: self))
             let position = t.location(in: self)
             let touchedNode = self.atPoint(position)
             if let name = touchedNode.name {
@@ -114,6 +170,9 @@ class GameScene: SKScene {
                     updateUser(direction: "right")
                 } else if name == "left" {
                     updateUser(direction: "left")
+                } else {
+                    attack = true
+                    attackStart = position
                 }
             }
         }
@@ -126,6 +185,14 @@ class GameScene: SKScene {
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchUp(atPoint: t.location(in: self))
             cancelMovements()
+            
+            if attack {
+                let dx = attackStart.x - t.location(in: self).x
+                let dy = attackStart.y - t.location(in: self).y
+                
+            
+                Fire(dx: dx, dy: dy)
+            }
         }
     }
     
@@ -144,9 +211,9 @@ class GameScene: SKScene {
     
     func moveUser() {
         if userLeft {
-            user.position.x -= 2.0
+            user.position.x -= 2.5
         } else if userRight {
-            user.position.x -= -2.0
+            user.position.x -= -2.5
         }
     }
     
--- HeroOne/HeroOne/Helpers/CharacterNode.swift
+++ HeroOne/HeroOne/Helpers/CharacterNode.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2018 Drew Foster. All rights reserved.
 //
 
+import UIKit
 import Foundation
+import SpriteKit
+import GameplayKit
+
+class CharacterNode: SKSpriteNode {
+   
+    var left = false
+    var right = false
+    
+    var hSpeed : CGFloat = 0.0
+    
+    var walkSpeed : CGFloat = 2.0
+    
+    
+    var stateMachine : GKStateMachine?
+    
+    func SetupStateMachine() {
+        let normalState = NormalState(with: self)
+        stateMachine = GKStateMachine(states: [normalState])
+        stateMachine?.enter(NormalState.self)
+    }
+}
