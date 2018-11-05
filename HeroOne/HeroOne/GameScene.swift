894a8678e01934b01eb31cbf7f52fdb4d21bb5f3
drfalcoew <drfalcoew@gmail.com>
2018-11-04 17:59:41 -0800

    added camera and updateCamera function

--- HeroOne/HeroOne/GameScene.swift
+++ HeroOne/HeroOne/GameScene.swift
@@ -12,14 +12,18 @@ import GameplayKit
 
 
 struct BitMasks {
-    
+    static let user : UInt32 = 0x1 << 0
+    static let floor : UInt32 = 0x1 << 1
+    static let fire : UInt32 = 0x1 << 2
+    static let enemy : UInt32 = 0x1 << 3
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var user : SKSpriteNode!
     var floor : SKSpriteNode!
-    
+    var one : SKShapeNode!
+    var two : SKShapeNode!
     
     var userLeft : Bool = false
     var userRight : Bool = false
@@ -33,15 +37,34 @@ class GameScene: SKScene {
     
     var tempNode : SKSpriteNode!
     
+    var cam: SKCameraNode?
     
     override func didMove(to view: SKView) {
         
         floor = childNode(withName: "floor") as? SKSpriteNode
         user = childNode(withName: "user") as? SKSpriteNode
         
-       
+        one = SKShapeNode(rectOf: CGSize(width: 50, height: 50))
+        two = SKShapeNode(rectOf: CGSize(width: 50, height: 50))
+        
+        one.fillColor = .red
+        two.fillColor = .red
+        
+        one.position = CGPoint(x: scene!.frame.width / -4, y: scene!.frame.height / 4)
+        two.position = CGPoint(x: scene!.frame.width / 4, y: scene!.frame.height / 4)
+        
+        self.addChild(one)
+        self.addChild(two)
+        
+        self.physicsWorld.contactDelegate = self
+
         self.view?.isMultipleTouchEnabled = true
         
+        cam = SKCameraNode()
+        self.addChild(cam!)
+        camera = cam
+        
+        UpdateCamera()
         SetupNotifications()
         SetupWeaponType()
         SetupAnimations()
@@ -49,6 +72,28 @@ class GameScene: SKScene {
         SetupPhysicsBodies()
     }
 
+    
+    
+    
+    func UpdateCamera() {
+        
+        if ((user.position.x < one.position.x) && (userLeft == true)) {
+            cam?.position.x -= 3.5
+            one.position.x -= 3.5
+            two.position.x -= 3.5
+            one.fillColor = .green
+            
+        } else if ((user.position.x > two.position.x) && (userRight == true)) {
+            cam?.position.x += 3.5
+            two.position.x += 3.5
+            one.position.x += 3.5
+            two.fillColor = .green
+            
+        } else {
+            one.fillColor = .red
+            two.fillColor = .red
+        }
+    }
     
     func SetupNotifications() {
         NotificationCenter.default.addObserver(self, selector: #selector(GameScene.updateUserLeft), name: NSNotification.Name(rawValue: "0"), object: nil)
@@ -58,7 +103,7 @@ class GameScene: SKScene {
     
     func SetupWeaponType() {
         let fireballTexture = SKTexture(imageNamed: "1")
-        fireball = SKSpriteNode(texture: fireballTexture, size: CGSize(width: user.size.height, height: user.size.height * 0.384))
+        fireball = SKSpriteNode(texture: fireballTexture, size: CGSize(width: user.size.height, height: user.size.height * 0.54))
     }
     
     func SetupAnimations() {
@@ -72,32 +117,61 @@ class GameScene: SKScene {
     }
     
     func SetupPhysicsBodies() {
-        floor?.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: floor.frame.width, height: floor.frame.height - floor.frame.height * 0.26))
         floor.physicsBody?.isDynamic = true
         floor.physicsBody?.pinned = true
-        floor.physicsBody?.collisionBitMask = 0
-        floor.physicsBody?.categoryBitMask = 1
+        floor.physicsBody?.allowsRotation = false
+        floor.physicsBody?.collisionBitMask = BitMasks.user
+        floor.physicsBody?.categoryBitMask = BitMasks.floor
+        floor.physicsBody?.contactTestBitMask = BitMasks.fire
         
         user.physicsBody = SKPhysicsBody(rectangleOf: user.size)
         user.physicsBody?.isDynamic = true
         user.physicsBody?.pinned = false
         user.physicsBody?.allowsRotation = false
         user.physicsBody?.affectedByGravity = true
-        user.physicsBody?.collisionBitMask = 1
-        user.physicsBody?.categoryBitMask = 0
+        user.physicsBody?.collisionBitMask = BitMasks.floor
+        user.physicsBody?.categoryBitMask = BitMasks.user
         
         fireball.physicsBody = SKPhysicsBody(texture: SKTexture(imageNamed: "1"), size: fireball.size)
         fireball.physicsBody?.isDynamic = true
         fireball.physicsBody?.pinned = false
         fireball.physicsBody?.allowsRotation = true
         fireball.physicsBody?.affectedByGravity = true
-        fireball.physicsBody?.collisionBitMask = 1
-        fireball.physicsBody?.categoryBitMask = 2
-        fireball.physicsBody?.density = 30
-        
+        fireball.physicsBody?.collisionBitMask = BitMasks.enemy
+        fireball.physicsBody?.categoryBitMask = BitMasks.fire
+        fireball.physicsBody?.contactTestBitMask = BitMasks.enemy | BitMasks.floor
+        fireball.physicsBody?.density = 2.6
         
     }
-    
+    func didBegin(_ contact: SKPhysicsContact) {
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        var a: SKSpriteNode? = nil
+        var b: SKSpriteNode? = nil
+        //var x : Int
+        
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        if firstBody.categoryBitMask == BitMasks.floor && secondBody.categoryBitMask == BitMasks.fire {
+            a = firstBody.node as! SKSpriteNode?
+            b = secondBody.node as! SKSpriteNode?
+            
+            b?.run(SKAction.fadeOut(withDuration: 0.1), completion: {
+                b?.removeFromParent()
+            })
+            
+            
+        }
+    }
+  
+            
     
     
     func touchDown(atPoint pos : CGPoint) {
@@ -134,16 +208,36 @@ class GameScene: SKScene {
     
     
     func Fire(dx : CGFloat, dy : CGFloat) {
-        print("In Fire Function")
+        
+        var newX = dx
+        var newY = dy
+        let maxPower : CGFloat = 350
+        
         if dx > 100 || dx < -100 || dy > 100 || dy < -100 {
+            if newX > maxPower {
+                newX = maxPower
+            } else if newX < -maxPower {
+                newX = -maxPower
+            }
+            if newY > maxPower {
+                newY = maxPower
+            }
             if let x = fireball {
                 x.position = user.position
                 tempNode = (x.copy() as! SKSpriteNode)
-                tempNode?.physicsBody?.affectedByGravity = false
+                tempNode?.physicsBody?.affectedByGravity = true
                 tempNode?.name = "fireball"
                 self.addChild(tempNode!)
-                tempNode?.run(SKAction.fadeIn(withDuration: 0.1))
-                tempNode.physicsBody?.applyImpulse(CGVector(dx: dx, dy: dy))
+                tempNode?.run(SKAction.fadeIn(withDuration: 0.25))
+                
+                if dx > 0 {
+                    tempNode.physicsBody?.applyAngularImpulse(dy / -25000)
+                } else {
+                    
+                    tempNode.xScale = -1
+                    tempNode.physicsBody?.applyAngularImpulse(dy / 25000)
+                }
+                tempNode.physicsBody?.applyImpulse(CGVector(dx: newX, dy: newY))
                 tempNode.run(SKAction.repeatForever(SKAction.animate(with: fireballArray, timePerFrame: 0.2)))
                 attack = false
             }
@@ -167,6 +261,15 @@ class GameScene: SKScene {
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches { self.touchMoved(toPoint: t.location(in: self))
+            //Flipping user whether aim is facing left or right
+            let position = t.location(in: self)
+            if position.x > attackStart.x {
+                user.texture = SKTexture(imageNamed: "mage")
+
+            } else {
+                user.texture = SKTexture(imageNamed: "mageRight")
+
+            }
             
         }
     }
@@ -176,6 +279,12 @@ class GameScene: SKScene {
             let dx = attackStart.x - t.location(in: self).x
             let dy = attackStart.y - t.location(in: self).y
             Fire(dx: dx, dy: dy)
+            //If we made variable to track aimDirection, it would remove the large chance of unnecessary texture overrides
+            if userRight == true {
+                updateUserRight()
+            } else if userLeft == true {
+                updateUserLeft()
+            }
         }
     }
     
@@ -187,14 +296,15 @@ class GameScene: SKScene {
     
     func moveUser() {
         if userLeft {
-            user.position.x -= 2.5
+            user.position.x -= 3.5
         } else if userRight {
-            user.position.x -= -2.5
+            user.position.x -= -3.5
         }
     }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         moveUser()
+        UpdateCamera()
     }
 }
